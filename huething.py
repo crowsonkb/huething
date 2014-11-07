2a41eeeb7dd078ce93ccb0be6466fcb1a5bb3fe1
Katherine Crowson <crowsonkb@gmail.com>
2014-11-06 19:21:32 -0800

    Add a color temperature flag, mired-shifted per individual bulb

--- huething.py
+++ huething.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 
 import argparse
+import colour
 import json
 import requests
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-b', '--brightness',
     help='the brightness scaling factor',
-    default=1.0,
+    default=1.,
     type=float)
 parser.add_argument('--host',
     help='the hostname/ip of the philips hue bridge',
     default='philips-hue')
+parser.add_argument('-k', '--temperature',
+    help='the color temperature in kelvins',
+    default=4000.,
+    type=float)
 parser.add_argument('-u', '--username',
     help='the username to act as when interacting with the bridge',
     required=True)
@@ -22,16 +27,23 @@ endpoint = 'http://{}/api/{}'.format(args.host, args.username)
 # TODO: check return status code, content, etc. What follows is basically only
 # a test.
 
-# Light 4 is the reference light because it is a bare bulb. The others need
-# mired shift applied due to lampshades. The desired white point is 4000K.
-settings = [(255, 225), (255, 225), (180, 245), (160, 250)]
-for index, setting in enumerate(settings):
-    url = '{}/lights/{}/state'.format(endpoint, index+1)
-    data=json.dumps({'bri': int(setting[0]*args.brightness), 'ct': setting[1]})
-    requests.put(url, data=data)
+# The first value in each tuple is the brightness scaling to apply. The second
+# value is the mired shift to apply. Light 4 is the reference light because it
+# is a bare bulb. The others need mired shift applied due to lampshades.
+settings = [
+    (1.0, -25),
+    (1.0, -25),
+    (0.7, -5),
+    (0.63, 0)
+]
 
-# known good state:
-# requests.put(endpoint+'/lights/1/state', data='{"bri":255,"ct":225}')
-# requests.put(endpoint+'/lights/2/state', data='{"bri":255,"ct":225}')
-# requests.put(endpoint+'/lights/3/state', data='{"bri":180,"ct":245}')
-# requests.put(endpoint+'/lights/4/state', data='{"bri":160,"ct":250}')
+computed_params = []
+for setting in settings:
+    bri = min(255, int(args.brightness * setting[0] * 255))
+    ct = 1000000. / (1000000./args.temperature + setting[1])
+    computed_params.append({'bri': bri, 'xy': colour.CCT_to_xy(ct)})
+
+for index, param in enumerate(computed_params):
+    url = '{}/lights/{}/state'.format(endpoint, index+1)
+    data = json.dumps(param)
+    requests.put(url, data=data)
