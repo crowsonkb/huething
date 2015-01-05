ea5770f30251affc086e4ec3b5e6971cfe59c8b5
Katherine Crowson <crowsonkb@gmail.com>
2015-01-04 19:44:33 -0800

    Rewrite to greatly reduce pylint complaints

--- huething.py
+++ huething.py
@@ -6,61 +6,69 @@ import json
 import requests
 import sys
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-b', '--brightness',
-    help='the brightness scaling factor',
-    default=1.,
-    type=float)
-parser.add_argument('-d', '--debug',
-    help='print debug information',
-    action='store_true')
-parser.add_argument('--host',
-    help='the hostname/ip of the philips hue bridge',
-    default='philips-hue')
-parser.add_argument('-k', '--temperature',
-    help='the color temperature in kelvins',
-    default=4000.,
-    type=float)
-parser.add_argument('-t', '--transition-time',
-    help='the transition time in seconds',
-    default=0.4,
-    type=float)
-parser.add_argument('-u', '--username',
-    help='the username to act as when interacting with the bridge',
-    required=True)
-
-args = parser.parse_args()
-endpoint = 'http://{}/api/{}'.format(args.host, args.username)
-
-def request(method, path, data=''):
-    if args.debug:
-        sys.stderr.write('{} {} {}\n'.format(method.__name__, path, data))
-    response = method(endpoint+path, data=data)
-    if args.debug:
-        sys.stderr.write('{} {}\n'.format(response.status_code, response.text))
-    return response
-
 # The first value in each tuple is the brightness scaling to apply. The second
 # value is the mired shift to apply. Light 4 is the reference light because it
 # is a bare bulb. The others need mired shift applied due to lampshades.
-settings = [
+SETTINGS = [
     (1.0, -25),
     (1.0, -25),
     (0.7, -5),
     (0.63, 0)
 ]
 
-computed_params = []
-for setting in settings:
-    bri = min(255, int(args.brightness * setting[0] * 255))
-    ct = 1000000. / (1000000./args.temperature + setting[1])
-    computed_params.append({
+class State():
+    def __init__(self):
+        self.args = None
+        self.endpoint = None
+
+__S = State()
+
+def request(method, path, data=''):
+    if __S.args.debug:
+        sys.stderr.write('{} {} {}\n'.format(method.__name__, path, data))
+    response = method(__S.endpoint+path, data=data)
+    if __S.args.debug:
+        sys.stderr.write('{} {}\n'.format(response.status_code, response.text))
+    return response
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-b', '--brightness',
+                        help='the brightness scaling factor',
+                        default=1., type=float)
+    parser.add_argument('-d', '--debug',
+                        help='print debug information',
+                        action='store_true')
+    parser.add_argument('--host',
+                        help='the hostname/ip of the philips hue bridge',
+                        default='philips-hue')
+    parser.add_argument('-k', '--temperature',
+                        help='the color temperature in kelvins',
+                        default=4000., type=float)
+    parser.add_argument('-t', '--transition-time',
+                        help='the transition time in seconds',
+                        default=0.4, type=float)
+    parser.add_argument('-u', '--username',
+                        help='the username to act as when interacting with the bridge',
+                        required=True)
+
+    __S.args = parser.parse_args()
+    __S.endpoint = 'http://{}/api/{}'.format(__S.args.host, __S.args.username)
+
+    computed_params = []
+    for setting in SETTINGS:
+        bri = min(255, int(__S.args.brightness * setting[0] * 255))
+        ct = 1000000. / (1000000./__S.args.temperature + setting[1])
+        computed_params.append({
             'bri': bri,
             'xy': colour.CCT_to_xy(ct),
-            'transitiontime': int(args.transition_time*10)
+            'transitiontime': int(__S.args.transition_time*10)
         })
 
-for index, param in enumerate(computed_params):
-    path = '/lights/{}/state'.format(index+1)
-    data = json.dumps(param)
-    request(requests.put, path, data)
+    for index, param in enumerate(computed_params):
+        path = '/lights/{}/state'.format(index+1)
+        data = json.dumps(param)
+        request(requests.put, path, data)
+
+if __name__ == '__main__':
+    main()
